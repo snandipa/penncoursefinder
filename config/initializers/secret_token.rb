c36d96f5024947b22afc2bf2ce1d8407327fe410
snandipa <mnandipa@gmail.com>
2012-04-11 12:59:53 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Penncoursefinder::Application.config.secret_token = '68a5fac8a8c2ad01ebf970929496bccaf9a58b316e4ef3e84c1fe3c01f40d5aec6f8e55f7c20e92f32df0554e0cc69f12cd28d258b098d08d3d901d024f58196'
